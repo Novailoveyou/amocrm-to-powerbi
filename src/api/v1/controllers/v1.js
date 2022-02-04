063b4639828a29cd2f363fe50c9664d0276d2e11
Novailoveyou <novailoveyoux@gmail.com>
2022-02-04 19:36:22 +0300

    edited v1 controller

--- src/api/v1/controllers/v1.js
+++ src/api/v1/controllers/v1.js
@@ -10,29 +10,24 @@ const { createCoreController } = require('@strapi/strapi').factories
 
 module.exports = createCoreController('api::v1.v1', ({ strapi }) => ({
   amocrm: async ctx => {
-    // let token
-    // const res = await axios.post(
-    //   `${process.env.AMOCRM_ROOT}/oauth2/access_token`,
-    //   {
-    //     client_id: process.env.AMOCRM_ID,
-    //     client_secret: process.env.AMOCRM_SECRET,
-    //     grant_type: process.env.AMOCRM_GRANT_TYPE,
-    //     code: process.env.AMOCRM_CODE,
-    //     redirect_uri: process.env.AMOCRM_REDIRECT_URI
-    //   }
-    // )
+    const res = await axios.post(
+      `${process.env.AMOCRM_ROOT}/oauth2/access_token`,
+      {
+        client_id: process.env.AMOCRM_ID,
+        client_secret: process.env.AMOCRM_SECRET,
+        grant_type: process.env.AMOCRM_GRANT_TYPE,
+        code: process.env.AMOCRM_CODE,
+        redirect_uri: process.env.AMOCRM_REDIRECT_URI
+      }
+    )
 
-    // const data = await res.data
+    const token = await res.data
 
-    // console.log(res)
-    // console.log(res.data)
-    strapi.query('api::amocrm-auth.amocrm-auth').create({
-      name: 'amocrmAuth',
-      content: [
-        {
-          token: { test: 'test' }
-        }
-      ]
+    const entry = await strapi.db.query('api::amocrm-auth.amocrm-auth').update({
+      where: { id: 1 },
+      data: {
+        token
+      }
     })
 
     ctx.body = 'Hello World!'
