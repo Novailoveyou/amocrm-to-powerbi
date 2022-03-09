cf59e54c2bab804df6b79131e722933bf05f61bf
Novailoveyou <novailoveyoux@gmail.com>
2022-03-09 18:32:57 +0300

    production deployment fix

--- config/database.js
+++ config/database.js
@@ -8,11 +8,9 @@ module.exports = ({ env }) => ({
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
       // ssl: env.bool('DATABASE_SSL'),
-      ssl: process.env.production
-        ? {
-            rejectUnauthorized: env.bool('DATABASE_SSL')
-          }
-        : env.bool('DATABASE_SSL'),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL')
+      },
       debug: false
     }
   }
