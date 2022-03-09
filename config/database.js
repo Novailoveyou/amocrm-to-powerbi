7b2361b01d004c361e641c293e142a6a7f8ced4d
Novailoveyou <novailoveyoux@gmail.com>
2022-03-09 16:39:32 +0300

    an attempt to make postgres work in production

--- config/database.js
+++ config/database.js
@@ -7,7 +7,11 @@ module.exports = ({ env }) => ({
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL')
+      // ssl: env.bool('DATABASE_SSL')
+      ssl: {
+        rejectUnauthorized: false
+      },
+      debug: false
     }
   }
 })
